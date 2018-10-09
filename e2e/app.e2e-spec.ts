a3f55b53f044d617349b12f5a673e333df26cfda
rupertlion <rupertlion@gmail.com>
2018-10-09 15:34:05 +0200

    Adding acceptance and unit tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -16,6 +16,12 @@ describe('App', () => {
       page.getTitle().then(title => {
         expect(title).toEqual('Ionic App');
       });
+
+      it('fill in form', () => {
+        page.fillInForm(1000, 'Female', 20);
+        expect(page.results_card_header()).toContain('Cooper Test Result');
+        expect(page.results_card_content()).toContain('Gender: female, Age: 20  Result: Poor');
+        });
     });
   })
 });
