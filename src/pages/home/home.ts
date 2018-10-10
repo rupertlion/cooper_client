ee92e87a37525f4e1a492b24c1c1939e6edb82f0
rupertlion <rupertlion@gmail.com>
2018-10-10 14:09:52 +0200

    Adding calculate to home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PersonProvider } from '../../providers/person/person';
 import { PerformanceDataProvider } from '../../providers/performance-data/performance-data';
+import { ModalController } from 'ionic-angular';
 
 @Component({
     selector: 'page-home',
@@ -12,17 +13,23 @@ export class HomePage {
     
     constructor(
       public navCtrl: NavController,
-      public person: PersonProvider
+      public person: PersonProvider,
+      public modalCtrl: ModalController,
+      public performanceData: PerformanceDataProvider //do I need this?
       ) {
       this.user = { distance: 1000, age: 20, gender: 'female' };
       }
       
-      calculate() {
+      calculate(user) {
         this.person.age = user.age;
         this.person.gender = user.gender;
         this.person.doAssessment(user.distance);
         this.performanceData
           .saveData({ performance_data: { data: { message: this.person.assessmentMessage}}})
           .subscribe(data => console.log(data));
+      }
+
+      showResults() {
+        this.modalCtrl.create(ResultsPage).present(); //how do I create ResultsPage?
       }
 }
