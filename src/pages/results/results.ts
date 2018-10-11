1d98c9d96b7c32e9146b177244f6080d514942be
rupertlion <rupertlion@gmail.com>
2018-10-11 10:44:48 +0200

    Updating results.ts for labelling

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { PerformanceDataProvider } from '../../providers/performance-data/performance-data';
+import { getLocaleNumberSymbol } from '../../../node_modules/@angular/common';
 
 @Component({
   selector: 'page-results',
@@ -21,9 +22,38 @@ export class ResultsPage {
     public navParams: NavParams) 
     {}
 
+  getLabels(collection: any) {
+    let uniqueLabels = [];
+  
+    collection.forEach(entry => {
+      if (entry.data.message && uniqueLabels.indexOf(entry.data.message) === -1) {
+        uniqueLabels.push(entry.data.message);
+      }
+    })
+  
+    return uniqueLabels;
+  }
+
+  getCount(collection:any, value:any) {
+    let count = 0;
+  
+    collection.forEach(entry => {
+      count += entry.data.message == value ? 1 : 0;
+    })
+  
+    return count;
+  }
+
   ionViewDidLoad() {
     this.performanceData
       .getResults()
-      .subscribe(data => (this.results = data.entries));
+      .subscribe(data => {
+        this.results = data.entries;
+        this.labels = this.getLabels(this.results);
+
+        this.labels.forEach(label => {
+          this.data.push(this.getCount(this.results, label));
+        })
+      });
   }
 }
